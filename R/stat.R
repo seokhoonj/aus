abc8880fbce57482a9a1fb4f6c07a5bee474a988
seokhoonj <seokhoonj@gmail.com>
2025-01-24 13:10:36 +0900

    `plot_stat_from_final_temp()` function is added

--- R/stat.R
+++ R/stat.R
@@ -60,3 +60,22 @@ plot_stat_from_final <- function(final, decis_var) {
     theme_view(x.angle = 90, y.size = 0, legend.position = "top", legend.justification = "left") +
     theme(legend.title = element_blank())
 }
+
+#' @export
+plot_stat_from_final_temp <- function(final, decis_var) {
+  jaid::assert_class(final, "data.table")
+  if (missing(decis_var))
+    decis_var <- local(.DECIS_VAR, envir = .AUS_ENV)
+  final[, (decis_var) := lapply(.SD, change_stat_decis), .SDcols = decis_var]
+  n <- nrow(final)
+  dm <- data.table::melt(final, id.vars = "id", value.vars = decis_var,
+                         variable.name = "coverage", value.name = "decis")
+  ds <- dm[, .(n = .N), .(coverage, decis)]
+  ds[, prop := n / sum(n), .(coverage)]
+  ggbar(ds, x = coverage, y = prop, ymax = max(prop) * 1.1, group = decis, fill = decis) +
+    scale_y_comma() +
+    labs(title = "Exact Measurement of Auto Underwriting System",
+         subtitle = sprintf("- %s Unit Test", jaid::as_comma(n))) +
+    theme_view(x.angle = 90, y.size = 0, legend.position = "top", legend.justification = "left") +
+    theme(legend.title = element_blank())
+}
